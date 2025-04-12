739da969e3f7d22009de8ba3486f15f5c06ea3eb
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-04-12 11:25:27 +0200

    resend to tg to array of recipients

--- src/reSendToTheTg.js
+++ src/reSendToTheTg.js
@@ -11,24 +11,26 @@ module.exports.reSendToTheTelegram = async function (to, from, subject, text, at
 
     for (const recipient of recipients) {
       if (recipient.includes(process.env.DOMEN)) {
-        const tgId = configData.forwardingRules.rcptToTg[recipient]
-        if (tgId) {
-          const tgMessage = `Received email from ${from} to ${recipient}\nSubject: ${subject}\n\n${text}`
-          await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
-            chat_id: tgId,
-            text: tgMessage,
-          })
-          logger.info(`Message sent to Telegram ID ${tgId}`)
-
-          for (const filePath of attachmentPaths) {
-            const formData = new FormData()
-            formData.append('chat_id', tgId)
-            formData.append('document', fs.createReadStream(filePath))
-
-            await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendDocument`, formData, {
-              headers: formData.getHeaders(),
+        const tgIds = configData.forwardingRules.rcptToTg[recipient]?.split(',').map(id => id.trim())
+        if (tgIds && tgIds.length > 0) {
+          for (const tgId of tgIds) {
+            const tgMessage = `Received email from ${from} to ${recipient}\nSubject: ${subject}\n\n${text}`
+            await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+              chat_id: tgId,
+              text: tgMessage,
             })
-            logger.info(`File sent to Telegram ID ${tgId}: ${filePath}`)
+            logger.info(`Message sent to Telegram ID ${tgId}`)
+
+            for (const filePath of attachmentPaths) {
+              const formData = new FormData()
+              formData.append('chat_id', tgId)
+              formData.append('document', fs.createReadStream(filePath))
+
+              await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendDocument`, formData, {
+                headers: formData.getHeaders(),
+              })
+              logger.info(`File sent to Telegram ID ${tgId}: ${filePath}`)
+            }
           }
         } else {
           logger.info(`No Telegram ID found for recipient ${recipient}`)
